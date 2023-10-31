58108672b5e7e925ce9ef5d6eb0f5047c77f4ba1
Airat2000 <gerasimovevgeni16082003@gmail.com>
2023-10-31 17:52:11 +0400

    fix: fix some bugs

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,37 +1,60 @@
-import { useState, useEffect } from 'react';
+import { Component, ChangeEvent } from 'react';
 
-function SearchBar({ onSearch }: { onSearch: (searchQuery: string) => void }) {
-  const [searchQuery, setSearchQuery] = useState<string>('');
+interface SearchBarProps {
+  onSearch: (searchQuery: string) => void;
+}
 
-  useEffect(() => {
+interface SearchBarState {
+  searchQuery: string;
+}
+
+class SearchBar extends Component<SearchBarProps, SearchBarState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+
+    this.state = {
+      searchQuery: '',
+    };
+  }
+
+  componentDidMount() {
     const savedSearchQuery = localStorage.getItem('searchQuery');
     if (savedSearchQuery) {
-      setSearchQuery(savedSearchQuery);
+      this.setState({ searchQuery: savedSearchQuery });
     }
-  }, []);
+  }
 
-  const handleSearch = () => {
-    onSearch(searchQuery.trim());
+  handleSearch = () => {
+    const { searchQuery } = this.state;
+    this.props.onSearch(searchQuery.trim());
 
     localStorage.setItem('searchQuery', searchQuery);
   };
 
-  return (
-    <div className="input-group mb-3">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Search..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <div className="input-group-append">
-        <button className="btn btn-primary" onClick={handleSearch}>
-          Search
-        </button>
+  handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  render() {
+    const { searchQuery } = this.state;
+
+    return (
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={this.handleInputChange}
+        />
+        <div className="input-group-append">
+          <button className="btn btn-primary" onClick={this.handleSearch}>
+            Search
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default SearchBar;
